259aa2b4ab1d3b5d0f3632ac65a9518a2352aac6
4knort <apple@apples-iMac-2.local>
2016-06-03 17:32:49 +0300

    if !formdata in window(fix) (for old i.e.)

--- build/js/script.js
+++ build/js/script.js
@@ -111,25 +111,31 @@
 
 
 })();
-var form = document.querySelector('.form');
-var inputSubmit = document.querySelector('.input-submit');
-var submitSuccess = document.querySelector('.input-submit__success');
+(function() {
+  if(!("FormData" in window)){
+    return;
+  }
+
+  var form = document.querySelector('.form');
+  var inputSubmit = document.querySelector('.input-submit');
+  var submitSuccess = document.querySelector('.input-submit__success');
 
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  var data = new FormData(form);
-  var xhr = new XMLHttpRequest;
+    var data = new FormData(form);
+    var xhr = new XMLHttpRequest;
 
-  xhr.open('GET', 'HTTP://localhost:8080/build/php/send.php');  
-  xhr.addEventListener("readystatechange", function(){
-      if(xhr.readyState == 4){
-        inputSubmit.style.backgroundColor = '#1BC2A6';  
-        inputSubmit.setAttribute('value', 'Send another message');
-        submitSuccess.classList.add('show'); 
-      }
-    }); 
-  xhr.send(data);
-  form.reset();    
-}) 
+    xhr.open('GET', 'HTTP://localhost:8080/build/php/send.php');  
+    xhr.addEventListener("readystatechange", function(){
+        if(xhr.readyState == 4){
+          inputSubmit.style.backgroundColor = '#1BC2A6';  
+          inputSubmit.setAttribute('value', 'Send another message');
+          submitSuccess.classList.add('show'); 
+        }
+      }); 
+    xhr.send(data);
+    form.reset();    
+  }) 
+})();
